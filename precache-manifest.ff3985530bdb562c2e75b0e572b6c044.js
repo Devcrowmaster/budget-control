c396051b3eac7025e6c6093b31777e206153dfbe
Crow <devcrowmaster@gmail.com>
2020-05-19 14:31:43 -0500

    Updates

---
+++ precache-manifest.ff3985530bdb562c2e75b0e572b6c044.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8d345bd872fa4a71759ce3fad8b73b",
+    "url": "/budget-control/index.html"
+  },
+  {
+    "revision": "6c6cafe0443b1d726dee",
+    "url": "/budget-control/static/css/main.480fbee7.chunk.css"
+  },
+  {
+    "revision": "03b6064696efeb5dbb8a",
+    "url": "/budget-control/static/js/2.72448ff5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/budget-control/static/js/2.72448ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6cafe0443b1d726dee",
+    "url": "/budget-control/static/js/main.f26c2048.chunk.js"
+  },
+  {
+    "revision": "7ed14ddfef3067ec90b0",
+    "url": "/budget-control/static/js/runtime-main.eb846db3.js"
+  }
+]);
